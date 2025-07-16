3ab1bbd7edc170768cb591ef63b91db4865a0e29
Adi <dihash@gmail.com>
2025-07-16 11:12:07 +0300

    Add css files

--- js/meme.controller.js
+++ js/meme.controller.js
@@ -1,5 +1,15 @@
 'use strict'
 
-function renderGallery(){
 
+function onInit() {
+    renderGallery()
+}
+
+function renderGallery() {
+    const elGallery = document.querySelector('.gallery-container')
+    var strGallery = ''
+    for (var i = 0; i < 18; i++) {
+        strGallery += `<img src="img/gallery/${i}.jpg" alt="">`
+    }
+    elGallery.innerHTML = strGallery
 }
