26e8a8156d3dde74f6425bca372fe380041d68d9
Adi <dihash@gmail.com>
2025-07-16 10:35:00 +0300

    App skeleton

---
+++ js/meme.service.js
@@ -0,0 +1,48 @@
+'use strict'
+
+var gImgs = [
+    {
+        id: 1,
+        url: 'img/1.jpg',
+        keywords: ['funny', 'cat']
+    }
+]
+
+var gMeme =
+{
+    selectedImgId: 5,
+    selectedLineIdx: 0,
+    lines: [
+        {
+            txt: 'I sometimes eat Falafel',
+            size: 20, color: 'red'
+        }
+    ]
+}
+var gKeywordSearchCountMap = {
+    'funny': 12,
+    'cat': 16,
+    'baby': 2
+}
+
+function _createImg() {
+    return {
+        id,
+        url,
+        keywords,
+    }
+}
+
+function _createMeme() {
+    return {
+        selectedImgId,
+        selectedLineIdx,
+        lines: [
+            {
+                txt,
+                size, 
+                color
+            }
+        ]
+    }
+}
